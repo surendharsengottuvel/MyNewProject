878530591f23ce3ba63fd725875277d921253cbd
surendharsengottuvel <surenthiranviru@gmail.com>
2023-02-19 23:50:41 -0800

    my scanner comit

---
+++ src/main/java/LearningOfJava/Scannerclass.java
@@ -0,0 +1,22 @@
+package LearningOfJava;
+
+import java.util.Scanner;
+
+public class Scannerclass
+{
+    public static void main(String[] args) {
+        Scanner blood=new Scanner(System.in);
+
+        System.out.println("what is your total marks");
+        int Mymarks=blood.nextInt();
+
+        System.out.println("what is your age");
+        int Myageis=blood.nextInt();
+
+
+
+
+
+
+    }
+}
